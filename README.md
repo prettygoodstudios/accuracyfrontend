d5911ff1d8b1c19afba7f077047105cc40502c34
Miguel Rust <hava12soft@gmail.com>
2018-11-20 19:28:46 -0700

    Finished building out styles for the Header.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# React/Redux Starter Project
-
-> Provided for the students of the [Bottega Code School](https://bottega.tech/)
-
-*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
+#Accuracy web app for accounting firm.
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Header = (props) => {
-  const {title, items} = props;
-  return(
-    <div className="header">
-      <div className="header__title">
-        <a href="#">{title}</a>
+class Header extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      mobileMenu: false
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('resize', () => {
+      if(window.innerWidth > 700){
+        this.setState({
+          mobileMenu: false
+        });
+      }
+    });
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({
+      mobileMenu: !this.state.mobileMenu
+    });
+  }
+
+  render(){
+    const {title, items} = this.props;
+    return(
+      <div className="header">
+        <div className="header__title">
+          <a href="#">{title}</a>
+        </div>
+        <ul className="header__menu" style={this.state.mobileMenu ? {display: "block"} : {}}>
+          { items.map((li, i) => {
+            return(
+              <li key={i} className="header__menu__item">
+                <a href="#">{li.title}</a>
+              </li>
+            )
+          })
+          }
+        </ul>
+        <div className="header__toggle">
+          <button onClick={this.toggleMobileMenu}>Menu</button>
+        </div>
       </div>
-      <ul className="header__menu">
-        { items.map((li, i) => {
-          return(
-            <li key={i} className="header__menu__item">
-              <a href="#">{li.title}</a>
-            </li>
-          )
-        })
-        }
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default Header;
